78efc222a8f745bb46a1f8596553fd514b875c04
Paul de Vries <pablo@blacktower.nl>
2015-10-31 18:35:05 +0000

    Update compatibility with 1.2.6 and 1.3.0

--- octoprint_touchui/static/js/includes/DOM.js
+++ octoprint_touchui/static/js/includes/DOM.js
@@ -310,11 +310,15 @@
 					if($('#control-jog-feedrate').length === 0) {
 						var jogPanels = $('#control > .jog-panel');
 
-						$(jogPanels[0]).attr("id", "control-jog-xy");
+						$(jogPanels[0]).find(".jog-panel:nth-child(1)").attr("id", "control-jog-xy");
+						$(jogPanels[0]).find(".jog-panel:nth-child(2)").attr("id", "control-jog-z");
 						$(jogPanels[1]).attr("id", "control-jog-extrusion");
 						$(jogPanels[2]).attr("id", "control-jog-general");
 
 						$('<div class="jog-panel" id="control-jog-feedrate"></div>').insertAfter($(jogPanels[2]));
+						$(jogPanels[0]).find("> button:last-child").prependTo("#control-jog-feedrate");
+						$(jogPanels[0]).find("> input:last-child").prependTo("#control-jog-feedrate");
+						$(jogPanels[0]).find("> .slider:last-child").prependTo("#control-jog-feedrate");
 
 					}
 
