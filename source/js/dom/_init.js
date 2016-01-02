4e92bb9dceeb4e856bdb3b425d80db70eb4b0838
Paul de Vries <pablo@blacktower.nl>
2016-01-02 21:20:39 +0100

    Move tabbar items count and active click event

--- source/js/dom/_init.js
+++ source/js/dom/_init.js
@@ -24,4 +24,12 @@ TouchUI.prototype.DOM.init = function() {
 		this.DOM.create.webcam.init( this.DOM.create.tabbar );
 	}
 
+	// Add class with how many tab-items
+	$("#tabs, #navbar").addClass("items-" + $("#tabs li:not(.hidden_touch)").length);
+
+	// Remove active class when clicking on a tab in the tabbar
+	$('#tabs [data-toggle=tab]').on("click", function() {
+		$("#all_touchui_settings").removeClass("item_active");
+	});
+
 }
--- source/js/dom/overwrite/tabbar.js
+++ source/js/dom/overwrite/tabbar.js
@@ -1,13 +1,5 @@
 TouchUI.prototype.DOM.overwrite.tabbar = function() {
 
-	// Add class with how many tab-items
-	$("#tabs, #navbar").addClass("items-" + $("#tabs li:not(.hidden_touch)").length);
-
-	// Remove active class when clicking on a tab in the tabbar
-	$('#tabs [data-toggle=tab]').on("click", function() {
-		$("#all_touchui_settings").removeClass("item_active");
-	});
-
 	// Force the webcam tab to load the webcam feed that original is located on the controls tab
 	$('#tabs [data-toggle=tab]').each(function(ind, elm) {
 
