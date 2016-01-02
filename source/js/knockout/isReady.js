4c65e40e66e2145316e278bc5ff2567b3d105694
Paul de Vries <pablo@blacktower.nl>
2016-01-02 18:44:35 +0100

    Merge isReady and isLoading knockout files

--- source/js/knockout/isReady.js
+++ source/js/knockout/isReady.js
@@ -8,6 +8,11 @@ TouchUI.prototype.knockout.isReady = function(touchViewModel, viewModels) {
 		gcodeFilesViewModel = viewModels[5],
 		navigationViewModel = viewModels[6];
 
+	// Repaint graph after resize (.e.g orientation changed)
+	$(window).on("resize", function() {
+		viewModels[8].updatePlot();
+	});
+
 	// Remove slimScroll from files list
 	$('.gcode_files').slimScroll({destroy: true});
 	$('.slimScrollDiv').slimScroll({destroy: true});
@@ -50,9 +55,8 @@ TouchUI.prototype.knockout.isReady = function(touchViewModel, viewModels) {
 
 	// Bind fullscreenChange to knockout
 	$(document).bind("fullscreenchange", function() {
-		self.isFullscreen = ($(document).fullScreen() !== false);
-		touchViewModel.isFullscreen(self.isFullscreen);
-		self.DOM.cookies.set("fullscreen", self.isFullscreen);
+		self.isFullscreen($(document).fullScreen() !== false);
+		self.DOM.cookies.set("fullscreen", self.isFullscreen());
 	});
 
 	// (Re-)Apply bindings to the new webcam div
@@ -102,37 +106,86 @@ TouchUI.prototype.knockout.isReady = function(touchViewModel, viewModels) {
 		}
 	});
 
-	// Force the webcam tab to load the webcam feed that original is located on the controls tab
-	$('#tabs a[data-toggle="tab"]').each(function(ind, elm) {
+	if( !self.isTouch ) {
+		gcodeFilesViewModel.listHelper.paginatedItems.subscribe(function(a) {
+			setTimeout(function() {
+				try {
+					self.scroll.iScrolls.body.refresh();
+				} catch(err) {
+					// Do nothing
+				};
+			}, 300);
+		});
+	}
 
-		// Get the currently attached events to the toggle
-		var events = $.extend([], jQuery._data(elm, "events").show),
-			$elm = $(elm);
-
-		// Remove all previous set events and call them after manipulating a few things
-		$elm.off("show").on("show", function(e) {
-			var scope = this,
-				current = e.target.hash,
-				previous = e.relatedTarget.hash;
-
-			current = (current === "#control") ? "#control_without_webcam" : current;
-			current = (current === "#webcam") ? "#control" : current;
-
-			previous = (previous === "#control") ? "#control_without_webcam" : previous;
-			previous = (previous === "#webcam") ? "#control" : previous;
-
-			// Call previous unset functions (e.g. let's trigger the event onTabChange in all the viewModels)
-			$.each(events, function(key, event) {
-				event.handler.call(scope, {
-					target: {
-						hash: current
-					},
-					relatedTarget: {
-						hash: previous
-					}
-				});
+	// Check if we can show whats new in this version
+	touchViewModel.settings.whatsNew.subscribe(function(whatsNew) {
+		if(whatsNew !== false && whatsNew.trim() != "") {
+			new PNotify({
+				title: 'TouchUI: What\'s new?',
+				text: whatsNew,
+				icon: 'glyphicon glyphicon-question-sign',
+				type: 'info',
+				hide: false
 			});
-		})
+		}
+	});
+
+	// Display any backend errors
+	touchViewModel.settings.error.subscribe(function(hasError) {
+		if(hasError !== false && hasError.trim() != "") {
+
+			// If Settings Modal is open, then block the hide of the modal once
+			if(settingsViewModel.settingsDialog.is(":visible")) {
+				var tmp = settingsViewModel.settingsDialog.modal;
+				settingsViewModel.settingsDialog.modal = function() {
+					settingsViewModel.settingsDialog.modal = tmp;
+				};
+			}
+
+			new PNotify({
+				title: 'TouchUI: Whoops, something went wrong...',
+				text: hasError,
+				icon: 'glyphicon glyphicon-question-sign',
+				type: 'error',
+				hide: false
+			});
+		}
+	});
+
+	// Reload CSS or LESS after saving our settings
+	var afterSettingsSave = ko.computed(function() {
+		return !settingsViewModel.receiving() && !settingsViewModel.sending() && touchViewModel.settingsUpdated();
+	});
+	afterSettingsSave.subscribe(function(settingsSaved) {
+		if(settingsSaved && !touchViewModel.settings.error()) {
+			var $less = $("#touchui-custom-less"),
+				$css = $("#touchui-css-only");
+
+			touchViewModel.settingsUpdated(false);
+			if(touchViewModel.settings.hasLESS()) {
+
+				if($less.length === 0) {
+					$('<link href="' + $("#data-touchui").attr("data-less") + '" rel="stylesheet/less" type="text/css" media="screen" id="touchui-custom-less">').appendTo("body");
+					less.sheets[0] = document.getElementById('touchui-custom-less');
+				}
+
+				$less.attr("href", $("#data-touchui").attr("data-less") + "?v=" + new Date().getTime());
+				$('style:not(#touch_updates_css)').remove();
+				$css.remove();
+				less.refresh();
+
+			} else {
+
+				if($css.length === 0) {
+					$('<link rel="stylesheet" type="text/css" media="screen" id="touchui-css-only">').appendTo("head").attr("href", $("#data-touchui").attr("data-css"));
+				}
+
+				$('style:not(#touch_updates_css)').remove();
+				$less.remove();
+
+			}
+		}
 	});
 
 }
