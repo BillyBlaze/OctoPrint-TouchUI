f4dd18e22624b561cdc17e77caf20027f26ef500
Paul de Vries <pablo@blacktower.nl>
2016-01-02 18:50:24 +0100

    Rename core.isLoading into core.init Add javascript errors within a notifaction Move tabbar overwrite to DOM.overwrite

---
+++ source/js/core/exception.js
@@ -0,0 +1,28 @@
+TouchUI.prototype.core.exception = function() {
+
+	var old = window.onerror || function() {};
+	window.onerror = function() {
+		old.call(this, arguments);
+		var text = '<p>Please help improving this plugin by <a href="https://github.com/BillyBlaze/OctoPrint-TouchUI/issues/new">submiting</a> the following error together with the OctoPrint version and browser you\'re using: </p><ul><li>';
+
+		if(arguments.length > 4) {
+			text += "<strong>" + arguments[4].message + "</strong><br>";
+			text += "<small>" + arguments[4].stack + "</small>";
+		} else {
+			text += "<strong>" + arguments[0] + "</strong><br>";
+			text += "<small>" + arguments[1] + " @ " + arguments[2] + "</small>";
+		}
+
+		text += "</li></ul>";
+
+		new PNotify({
+			title: 'TouchUI: Javascript error...',
+			text:  text,
+			icon: 'glyphicon glyphicon-question-sign',
+			type: 'error',
+			hide: false,
+			confirm: false
+		});
+
+	}
+}
---
+++ source/js/dom/overwrite/tabbar.js
@@ -0,0 +1,36 @@
+TouchUI.prototype.DOM.overwrite.tabbar = function() {
+
+	// Force the webcam tab to load the webcam feed that original is located on the controls tab
+	$('#tabs a[data-toggle="tab"]').each(function(ind, elm) {
+
+		// Get the currently attached events to the toggle
+		var events = $.extend([], jQuery._data(elm, "events").show),
+			$elm = $(elm);
+
+		// Remove all previous set events and call them after manipulating a few things
+		$elm.off("show").on("show", function(e) {
+			var scope = this,
+				current = e.target.hash,
+				previous = e.relatedTarget.hash;
+
+			current = (current === "#control") ? "#control_without_webcam" : current;
+			current = (current === "#webcam") ? "#control" : current;
+
+			previous = (previous === "#control") ? "#control_without_webcam" : previous;
+			previous = (previous === "#webcam") ? "#control" : previous;
+
+			// Call previous unset functions (e.g. let's trigger the event onTabChange in all the viewModels)
+			$.each(events, function(key, event) {
+				event.handler.call(scope, {
+					target: {
+						hash: current
+					},
+					relatedTarget: {
+						hash: previous
+					}
+				});
+			});
+		})
+	});
+
+}
