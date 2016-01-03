79c4308ebb8b15d723040cd990e8fa4d2a7e8581
Paul de Vries <pablo@blacktower.nl>
2016-01-03 18:09:15 +0100

    Add support of min-height when toggling sub-dropdowns and target active container

--- octoprint_touchui/static/js/touchui.bundled.js
+++ octoprint_touchui/static/js/touchui.bundled.js
@@ -107,7 +107,7 @@ TouchUI.prototype.components.dropdown = {
 			.off('.dropdown')
 			.on('touchstart.dropdown.data-api', '.dropdown-menu', function (e) { e.stopPropagation() })
 			.on('click.dropdown.data-api', '[data-toggle=dropdown]', function(e) {
-				var $dropdownToggle = $(e.target),
+				var $dropdownToggle = $(e.currentTarget),
 					$dropdownContainer = $dropdownToggle.parent();
 
 				// Stop the hashtag from propagating
@@ -116,6 +116,9 @@ TouchUI.prototype.components.dropdown = {
 				// Toggle the targeted dropdown
 				$dropdownContainer.toggleClass("open");
 
+				// Refresh current scroll and add a min-height so we can reach the dropdown if needed
+				self.components.dropdown.containerMinHeight.call(self, $dropdownContainer, $dropdownToggle);
+
 				// Skip everything if we are in a dropdown toggling a dropdown (one click event is enuff!)
 				if( $dropdownContainer.parents('.open > .dropdown-menu').length > 0 ) {
 					return;
@@ -123,18 +126,6 @@ TouchUI.prototype.components.dropdown = {
 
 				// Remove all other active dropdowns
 				$('.open [data-toggle="dropdown"]').not($dropdownToggle).parent().removeClass('open');
-
-				// Refresh current scroll and add a min-height so we can reach the dropdown if needed
-				if ( !self.isTouch ) {
-					setTimeout(function() {
-						var y = Math.abs(self.scroll.currentActive.y),
-							height = $dropdownToggle.next().outerHeight(),
-							top = $dropdownToggle.next().offset().top;
-
-						$('.octoprint-container').css("min-height", y + top + height);
-						self.scroll.currentActive.refresh();
-					}, 0);
-				}
 
 				$(document).off("click"+namespace).on("click"+namespace, function(event) {
 					// Check if we scrolled (touch devices wont trigger this click event after scrolling so assume we didn't move)
@@ -165,6 +156,37 @@ TouchUI.prototype.components.dropdown = {
 	toggleSubmenu: function() {
 		$(".dropdown-submenu").addClass("dropdown");
 		$(".dropdown-submenu > a").attr("data-toggle", "dropdown");
+	},
+
+	// Refresh current scroll and add a min-height so we can reach the dropdown if needed
+	containerMinHeight: function($dropdownContainer, $dropdownToggle) {
+		var self = this;
+
+		// Touch devices can reach the dropdown by CSS, only if we're using iScroll
+		if ( !self.isTouch ) {
+			// Get active container
+			var $container = ($dropdownContainer.parents('.modal').length === 0 ) ? $('.octoprint-container') : $dropdownContainer.parents('.modal .modal-body');
+
+			// If we toggle within the dropdown then get the parent dropdown for total height
+			var $dropdownMenu = ( $dropdownContainer.parents('.open > .dropdown-menu').length > 0 ) ? $dropdownContainer.parents('.open > .dropdown-menu') : $dropdownToggle.next();
+
+			setTimeout(function() {
+
+				//If the main dropdown has closed (by toggle) then let's remove the min-height else
+				if(!$dropdownMenu.parent().hasClass("open")) {
+					$container.css("min-height", 0);
+					self.scroll.currentActive.refresh();
+				} else {
+					var y = Math.abs(self.scroll.currentActive.y),
+						height = $dropdownMenu.outerHeight(),
+						top = $dropdownMenu.offset().top;
+
+					$container.css("min-height", y + top + height);
+					self.scroll.currentActive.refresh();
+				}
+
+			}, 0);
+		}
 	}
 
 }
