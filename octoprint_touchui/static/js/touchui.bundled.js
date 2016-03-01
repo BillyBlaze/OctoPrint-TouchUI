1044a40266a4d9b9e7fccf53272be2f94b5217c6
Paul de Vries <pablo@blacktower.nl>
2016-03-01 00:29:58 +0000

    :art: Clean bootstrap Clean bootstrap by moving some parts to bridge.js

--- octoprint_touchui/static/js/touchui.bundled.js
+++ octoprint_touchui/static/js/touchui.bundled.js
@@ -740,7 +740,25 @@ TouchUI.prototype.core.bridge = function() {
 	var self = this;
 
 	this.core.bridge = {
+
 		allViewModels: {},
+		TOUCHUI_REQUIRED_VIEWMODELS: [
+			"terminalViewModel",
+			"connectionViewModel",
+			"settingsViewModel",
+			"softwareUpdateViewModel",
+			"controlViewModel",
+			"gcodeFilesViewModel",
+			"navigationViewModel",
+			"pluginManagerViewModel",
+			"temperatureViewModel",
+			"loginStateViewModel"
+		],
+		TOUCHUI_ELEMENTS: [
+			"#touchui_settings_dialog",
+			"#settings_plugin_touchui",
+			"#navbar_plugin_touchui"
+		],
 
 		domLoading: function() {
 			if (self.isActive()) {
@@ -751,6 +769,11 @@ TouchUI.prototype.core.bridge = function() {
 
 		domReady: function() {
 			if (self.isActive()) {
+
+				if(_.some(self.core.bridge.OCTOPRINT_VIEWMODELS, function(v) { return v[2] === "#gcode"; })) {
+					self.core.bridge.TOUCHUI_REQUIRED_VIEWMODELS = self.core.bridge.TOUCHUI_REQUIRED_VIEWMODELS.concat(["gcodeViewModel"]);
+				}
+
 				self.components.dropdown.init.call(self);
 				self.components.fullscreen.init.call(self);
 				self.components.keyboard.init.call(self);
@@ -763,7 +786,7 @@ TouchUI.prototype.core.bridge = function() {
 		},
 
 		koStartup: function TouchUIViewModel(viewModels) {
-			self.core.bridge.allViewModels = _.object(TOUCHUI_REQUIRED_VIEWMODELS, viewModels);
+			self.core.bridge.allViewModels = _.object(self.core.bridge.TOUCHUI_REQUIRED_VIEWMODELS, viewModels);
 			self.knockout.isLoading.call(self, self.core.bridge.allViewModels);
 			return self;
 		}
--- source/js/bootstrap.js
+++ source/js/bootstrap.js
@@ -4,30 +4,13 @@
 	Touch.domLoading();
 
 	$(function() {
-		TOUCHUI_REQUIRED_VIEWMODELS = [
-			"terminalViewModel",
-			"connectionViewModel",
-			"settingsViewModel",
-			"softwareUpdateViewModel",
-			"controlViewModel",
-			"gcodeFilesViewModel",
-			"navigationViewModel",
-			"pluginManagerViewModel",
-			"temperatureViewModel",
-			"loginStateViewModel"
-		];
-
-		if(_.some(OCTOPRINT_VIEWMODELS, function(v) { return v[2] === "#gcode"; })) {
-			TOUCHUI_REQUIRED_VIEWMODELS = TOUCHUI_REQUIRED_VIEWMODELS.concat(["gcodeViewModel"]);
-		}
-
 		Touch.domReady();
 
 		OCTOPRINT_VIEWMODELS.push([
 			Touch.koStartup,
-			TOUCHUI_REQUIRED_VIEWMODELS,
-			["#touchui_settings_dialog", "#settings_plugin_touchui", "#navbar_plugin_touchui"],
-			TOUCHUI_REQUIRED_VIEWMODELS
+			Touch.TOUCHUI_REQUIRED_VIEWMODELS,
+			Touch.TOUCHUI_ELEMENTS,
+			Touch.TOUCHUI_REQUIRED_VIEWMODELS
 		]);
 	});
 
