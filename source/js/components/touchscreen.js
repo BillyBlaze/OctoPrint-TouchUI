4cc3a14a07dc725d6fc37b381e12d7acafcc135d
Paul de Vries <pablo@blacktower.nl>
2016-06-13 17:41:16 +0200

    :bug: Disable hasTouch false on Touchscreen
    Touchscreen devices (e.g. Raspberry Pi Offical) no longer use mouse events and emit touch events; if hasTouch is set to false, then iScroll will not detect touch events and scrolling will not work. Fixes #46

--- source/js/components/touchscreen.js
+++ source/js/components/touchscreen.js
@@ -2,7 +2,7 @@ TouchUI.prototype.components.touchscreen = {
 
 	init: function () {
 		$("html").addClass("isTouchscreenUI");
-		this.settings.hasTouch = false;
+		//this.settings.hasTouch = false;
 		this.settings.isTouchscreen(true);
 
 		if (this.settings.isEpiphanyOrKweb) {
