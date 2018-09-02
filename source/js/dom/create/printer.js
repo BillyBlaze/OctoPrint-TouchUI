d5c5af371f799180c44187506f8ed8295504bc38
Paul de Vries <pablo@blacktower.nl>
2018-09-02 21:36:39 +0000

    :art: Add upload button
    Allow users to upload files without scrolling through all the files
    
    Fixes #210

--- source/js/dom/create/printer.js
+++ source/js/dom/create/printer.js
@@ -20,6 +20,19 @@ TouchUI.prototype.DOM.create.printer = {
 		// Move the contents of the hidden accordions to the new print status and files tab
 		this.move.$state.appendTo(this.container.$elm.find(".row-fluid"));
 		this.move.$files.insertAfter(this.container.$elm.find(".row-fluid #state_wrapper"));
+
+		// Create an upload button in the header
+		$('<div class="upload-trigger accordion-heading-button btn-group" data-bind="enable: $root.loginState.isUser(), css: {disabled: !$root.loginState.isUser()}">' +
+			'<a href="#" title="' + $('.upload-buttons .fileinput-button:first-child > span').text()  +'">' +
+				'<i class="fa fa-upload"></i>' +
+			'</a>' +
+		'</div>')
+			.appendTo('#files_wrapper .accordion-heading')
+			.find('a[href="#"]')
+			.on('click', function(e) {
+				e.preventDefault();
+				$('#gcode_upload').click();
+			});
 	}
 
 }
