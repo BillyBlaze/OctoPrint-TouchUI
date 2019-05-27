22d2023bd3fb20e23afe40bb1050f24f347bf58e
BillyBlaze <BillyBlaze@users.noreply.github.com>
2019-05-27 17:13:05 +0000

    :bug: Fix Shutdown Printer Plugin
    While disabling Themify the Shutdown Printer Plugin no longer works
    
    Fixes #302

--- octoprint_touchui/static/js/touchui.bundled.js
+++ octoprint_touchui/static/js/touchui.bundled.js
@@ -1524,7 +1524,7 @@ TouchUI.prototype.plugins.tempsGraph = function() {
 TouchUI.prototype.plugins.themify = function() {
 
 	_.remove(OCTOPRINT_VIEWMODELS, function(obj) {
-		if (obj[2] && obj[2].indexOf("#settings_plugin_themeify") !== -1) {
+		if (obj[2] && _.isArray(obj[2]) && obj[2].indexOf("#settings_plugin_themeify") !== -1) {
 			console.info("TouchUI: Themeify is disabled while TouchUI is active.");
 			return true;
 		}
